e6aedb19c1a9e4071dee70abf23302e2f6f4f8af
Adam Duff <j.adam.duff@gmail.com>
2018-08-13 11:03:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   array.any? {|val| val.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? {|val| cheese_types.include?(val)}
 end
