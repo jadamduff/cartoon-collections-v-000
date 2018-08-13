a49c12c506d0eb24305ff52e877c06d33638061e
Adam Duff <j.adam.duff@gmail.com>
2018-08-13 11:14:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |val|
-    return val if cheese_types.include?(val) == true
+    return val if cheese_types.include?(val)
   end
   return nil
 end
